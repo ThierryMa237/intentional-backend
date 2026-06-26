feede2dda99fa0385684b765001259db6ee29863
ThierryMa237 <thierry-manuel.zibi-ondobo@student.junia.com>
2026-06-26 10:08:15 +0200

    Reparation du fichier database.py pour le déploiement sur Railway

--- app/database.py
+++ app/database.py
@@ -5,7 +5,11 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = (
+    os.getenv("DATABASE_URL") or
+    os.getenv("DATABASE_PUBLIC_URL") or
+    os.getenv("POSTGRES_URL")
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
